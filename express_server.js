b9259334817151e64d4b4c57a1dd0721607aa6c7
Evan Quirk <evan_quirk@me.com>
2021-02-15 20:24:29 -0800

    refactor: clean up test code and comment routes for visibility

--- express_server.js
+++ express_server.js
@@ -1,26 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+// landing page route
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
-
+// urls page route - check database
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+// new url page route
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+ // short url view route - pull long url from database.
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -28,16 +33,9 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+// Test Route for database:
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 
