22c6c3728295adf2bbf4cf08fc0377f96c0b1d46
Evan Quirk <evan_quirk@me.com>
2021-02-15 21:01:44 -0800

    feat: add generate Random String function for express server

--- express_server.js
+++ express_server.js
@@ -12,20 +12,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(6);
+};
+
 // landing page route
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 // urls page route - check database
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 // new url page route
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
- // short url view route - pull long url from database.
+// short url view route - pull long url from database.
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
