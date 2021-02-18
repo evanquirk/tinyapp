07f40f8e67a130bc3be249b3f80ca3fa83b8fda0
Evan Quirk <evan_quirk@me.com>
2021-02-17 16:40:22 -0800

    chore: swap cookie value for username. give user a random generated id

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const { generateRandomString , checkEmail } = require('./helper');
 
 const app = express();
 const PORT = 8080;
@@ -14,8 +15,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const generateRandomString = function() {
-  return Math.random().toString(36).substring(6, 12);
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
 
 // landing page route
@@ -23,36 +33,49 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
 //get cookies
-app.get('/', function (req, res) {
-  // Cookies that have not been signed
+app.get('/', function(req, res) {
   console.log('Cookies: ', req.cookies);
-  // Cookies that have been signed
   console.log('Signed Cookies: ', req.signedCookies);
 });
 
+
 // urls page route - check database
 app.get("/urls", (req, res) => {
+  const user = users[req.cookies["user_id"]];
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user
   };
   res.render("urls_index", templateVars);
 });
 
-// new url page route
-app.get("/urls/new", (req, res) => {
+app.get("/register", (req, res) => {
+  const user = users[req.cookies["user_id"]];
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user
+  };
+  res.render("urls_register", templateVars);
+});
+
+// new url page route
+app.get("/urls/new", (req, res) => {
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {
+    urls: urlDatabase,
+    user
   };
   res.render("urls_new", templateVars);
 });
+
 // short url view route - pull long url from database
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL, longURL , username: req.cookies["username"] };
+  const user = users[req.cookies["user_id"]];
+  const templateVars = { shortURL, longURL , user };
   res.render("urls_show", templateVars);
 });
 
@@ -60,6 +83,19 @@ app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
+});
+
+app.post("/register", (req, res) => {
+  const rID = generateRandomString();
+  users[rID] = {id: rID, email: req.body.email, password: req.body.password };
+
+  if (!req.body.email || !req.body.password || checkEmail(req.body.email, users)) {
+    res.status(400); res.send("Bad Gateway. No Email Or Password, or Email in use.");
+  } else {
+    res.cookie("user_id", rID);
+    res.redirect("/urls");
+    console.log(users[rID]);
+  }
 });
 
 
@@ -71,7 +107,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
   //set a cookie to username (value in the request body)
   //redirect back to /urls page
